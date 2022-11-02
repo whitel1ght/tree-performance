0070a5a4f39284ca2ba04222dad15fe27980b1f9
Dmitry Mamyrev <dmitry.mamyrev@epicmax.co>
2022-11-02 10:46:06 +0300

    vuestic installed

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createVuestic } from 'vuestic-ui'
+import 'vuestic-ui/css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+// add vuestic-ui
+app.use(createVuestic())
+
+app.mount('#app')
